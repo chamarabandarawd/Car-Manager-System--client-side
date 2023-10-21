0eb26d01730d4ce5d4cdecc59439eadeae768263
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-21 18:31:05 +0530

    revised for deploying

--- src/pages/van/Vans.jsx
+++ src/pages/van/Vans.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import "./styles.css"
 import { NavLink } from 'react-router-dom'
-import CircularProgress from '@mui/material/CircularProgress';
 
 const Vans = () => {
   const [vans, setVans] = useState([])
